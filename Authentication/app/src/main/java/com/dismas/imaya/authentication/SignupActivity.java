ac41fdf5a7c282542e690475661ca6587a8c4d64
ImayaDismas <imayadismas@gmail.com>
2016-08-20 18:16:48 +0300

    Edited the layouts

--- Authentication/app/src/main/java/com/dismas/imaya/authentication/SignupActivity.java
+++ Authentication/app/src/main/java/com/dismas/imaya/authentication/SignupActivity.java
@@ -90,7 +90,8 @@ public class SignupActivity extends AppCompatActivity {
     public void onSignupSuccess() {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
-        finish();
+        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 
     public void onSignupFailed() {
@@ -107,21 +108,21 @@ public class SignupActivity extends AppCompatActivity {
         String password = _passwordText.getText().toString();
 
         if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
+            _nameText.setError("At least 3 characters");
             valid = false;
         } else {
             _nameText.setError(null);
         }
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            _emailText.setError("Enter a valid email address");
             valid = false;
         } else {
             _emailText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 4 || password.length() > 15) {
+            _passwordText.setError("Between 4 and 15 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
